22219fa2aeafff11484562b7f604a87223914edd
how <370969878@qq.com>
2018-04-26 09:05:01 +0800

    bug 修改

--- android/src/main/java/com/mg/app/Compression.java
+++ android/src/main/java/com/mg/app/Compression.java
@@ -1,8 +1,10 @@
 package com.mg.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableMap;
@@ -15,33 +17,45 @@ import id.zelory.compressor.Compressor;
 
 class Compression {
 
-    public File compressImage(final Activity activity, final ReadableMap options, final String originalImagePath) throws IOException {
+    public File compressImage(final Context activity, final ReadableMap options, final String originalImagePath) throws IOException {
         Integer maxWidth = options.hasKey("width") ? options.getInt("width") : null;
         Integer maxHeight = options.hasKey("height") ? options.getInt("height") : null;
         Integer quality = options.hasKey("compressQuality") ? options.getInt("compressQuality") : null;
 
-        if (maxWidth == null || maxWidth<=0) {
+
+        if (maxWidth == null || maxWidth <= 0) {
             maxWidth = 300;
         }
-        if (maxHeight == null || maxHeight<=0 ) {
+        if (maxHeight == null || maxHeight <= 0) {
             maxHeight = 300;
         }
         if (quality == null || quality >= 100 || quality <= 0) {
             return new File(originalImagePath);
         }
+        String path = PickerModule.img_savepath.substring(0, PickerModule.img_savepath.length() - 1);
         Compressor compressor = new Compressor(activity)
                 .setCompressFormat(Bitmap.CompressFormat.JPEG)
-                .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES).getAbsolutePath());
+                .setDestinationDirectoryPath(path);
+//                .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(
+//                        Environment.DIRECTORY_PICTURES).getAbsolutePath());
 
-        compressor.setQuality(quality);
+
+        Integer myquality = 80;
+        if (quality < myquality) {
+            myquality = quality;
+        }
+        compressor.setQuality(myquality);
 
         if (maxWidth != null) {
             compressor.setMaxWidth(maxWidth);
+            Log.d("aaa", maxWidth + "---maxWidth2");
+
         }
 
         if (maxHeight != null) {
             compressor.setMaxHeight(maxHeight);
+            Log.d("aaa", maxHeight + "---maxHeight2");
+
         }
 
         File image = new File(originalImagePath);
@@ -49,7 +63,7 @@ class Compression {
         String[] paths = image.getName().split("\\.(?=[^\\.]+$)");
         String compressedFileName = paths[0] + "-compressed";
 
-        if(paths.length > 1)
+        if (paths.length > 1)
             compressedFileName += "." + paths[1];
 
         return compressor
