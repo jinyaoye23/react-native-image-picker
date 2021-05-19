4b65527116ef2c57f9127f37b15eb359dad92596
Stephen <jinyaoye23@126.com>
2021-05-19 11:04:47 +0800

    解决部分安卓手机（红米note5, note8, 三星手机）签到拍照图片旋转的问题

--- android/src/main/java/com/mg/app/PickerModule.java
+++ android/src/main/java/com/mg/app/PickerModule.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.media.ExifInterface;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
@@ -326,7 +327,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
                     // oriPhotoFile);
                     uri = FileProvider.getUriForFile(activity, authorities, oriPhotoFile);
                     /** modify by david at 2019-8-6 start */
-                    }
+                }
                 intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);//私有目录读写权限
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
                 activity.startActivityForResult(intent, TAKE_IMAGE_REQUEST_CODE);
@@ -425,6 +426,70 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
                 return "";
         }
     }
+
+    /* add by Stpehen at 2021-05-19 start */
+
+    /**
+     * 读取图片的旋转的角度
+     *
+     * @param path
+     *            图片绝对路径
+     * @return 图片的旋转角度
+     */
+    private int getBitmapDegree(String path) {
+        int degree = 0;
+        try {
+            // 从指定路径下读取图片，并获取其EXIF信息
+            ExifInterface exifInterface = new ExifInterface(path);
+            // 获取图片的旋转信息
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+
+    /**
+     * 将图片按照某个角度进行旋转
+     *
+     * @param bm
+     *            需要旋转的图片
+     * @param degree
+     *            旋转角度
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotateBitmapByDegree(Bitmap bm, int degree) {
+        Bitmap returnBm = null;
+
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+        }
+        if (returnBm == null) {
+            returnBm = bm;
+        }
+        if (bm != returnBm) {
+            bm.recycle();
+        }
+        return returnBm;
+    }
+    /* add by Stpehen at 2021-05-19 end */
 
     float texts;
 
@@ -659,6 +724,13 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
 
             FileInputStream fis = new FileInputStream(path);
             Bitmap bitmap = BitmapFactory.decodeStream(fis);
+            /* add by Stephen at 2021-05-19 start*/
+            // 解决Android部分手机（红米note5 note8 和部分三星手机）上图片旋转问题
+            Integer degree = getBitmapDegree(path);
+            if (degree != 0) {
+                bitmap = rotateBitmapByDegree(bitmap, degree);
+            }
+            /* add by Stephen at 2021-05-19 end*/
 
             if (bitmap == null) {
                 return;
